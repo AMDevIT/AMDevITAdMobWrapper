7e6bf42b31f2cd2257d4661f8fd68327789df109
Alessandro Morvillo <a.morvillo@amdev.it>
2026-04-21 19:42:06 +0200

    Fixed ApiDefinition to correcly bind framework
    The API Definition now correctly bind framework

--- sources/dotnet/AMDevIT.Admob.Wrapper/AMDevIT.Admob.Wrapper.iOSNative/ApiDefinition.cs
+++ sources/dotnet/AMDevIT.Admob.Wrapper/AMDevIT.Admob.Wrapper.iOSNative/ApiDefinition.cs
@@ -2,13 +2,115 @@ using Foundation;
 using ObjCRuntime;
 using UIKit;
  
-
-namespace AMDevIT.Admob.Wrapper
+namespace AMDevIT.Admob.Wrapper.iOSNative
 {
+		// @protocol OnAdEventListener
+	/*
+  Check whether adding [Model] to this declaration is appropriate.
+  [Model] is used to generate a C# class that implements this protocol,
+  and might be useful for protocols that consumers are supposed to implement,
+  since consumers can subclass the generated class instead of implementing
+  the generated interface. If consumers are not supposed to implement this
+  protocol, then [Model] is redundant and will generate code that will never
+  be used.
+*/[Protocol (Name = "_TtP12AdMobWrapper17OnAdEventListener_")]
+[Model(Name="OnAdEventListener")]
+[BaseType(typeof(NSObject))]
+	public interface OnAdEventListener
+	{
+		// @required -(void)onAdShown;
+		[Export ("onAdShown")]
+		void OnAdShown ();
+
+		// @required -(void)onAdDismissed;
+		[Export ("onAdDismissed")]
+		void OnAdDismissed ();
+
+		// @required -(void)onAdClicked;
+		[Export ("onAdClicked")]
+		void OnAdClicked ();
+
+		// @required -(void)onAdImpression;
+		[Export ("onAdImpression")]
+		void OnAdImpression ();
+
+		// @required -(void)onAdFailedToShowWithErrorCode:(NSInteger)errorCode errorMessage:(NSString * _Nonnull)errorMessage;
+		[Export ("onAdFailedToShowWithErrorCode:errorMessage:")]
+		void OnAdFailedToShowWithErrorCode (nint errorCode, string errorMessage);
+	}
+
+	// @protocol OnAdLoadedListener
+	/*
+  Check whether adding [Model] to this declaration is appropriate.
+  [Model] is used to generate a C# class that implements this protocol,
+  and might be useful for protocols that consumers are supposed to implement,
+  since consumers can subclass the generated class instead of implementing
+  the generated interface. If consumers are not supposed to implement this
+  protocol, then [Model] is redundant and will generate code that will never
+  be used.
+*/[Protocol (Name = "_TtP12AdMobWrapper18OnAdLoadedListener_")]
+[Model(Name="OnAdLoadedListener")]
+[BaseType(typeof(NSObject))]
+	public interface OnAdLoadedListener
+	{
+		// @required -(void)onAdLoaded;
+		[Export ("onAdLoaded")]
+		void OnAdLoaded ();
+
+		// @required -(void)onAdFailedToLoadWithErrorCode:(NSInteger)errorCode errorMessage:(NSString * _Nonnull)errorMessage;
+		[Export ("onAdFailedToLoadWithErrorCode:errorMessage:")]
+		void OnAdFailedToLoadWithErrorCode (nint errorCode, string errorMessage);
+	}
+
+	// @protocol OnInitializedListener
+	/*
+  Check whether adding [Model] to this declaration is appropriate.
+  [Model] is used to generate a C# class that implements this protocol,
+  and might be useful for protocols that consumers are supposed to implement,
+  since consumers can subclass the generated class instead of implementing
+  the generated interface. If consumers are not supposed to implement this
+  protocol, then [Model] is redundant and will generate code that will never
+  be used.
+*/[Protocol (Name = "_TtP12AdMobWrapper21OnInitializedListener_")]
+[Model(Name="OnInitializedListener")]
+[BaseType(typeof(NSObject))]
+	public interface OnInitializedListener
+	{
+		// @required -(void)onInitialized;
+		
+		[Export ("onInitialized")]
+		void OnInitialized ();
+
+		// @required -(void)onInitializationFailedWithError:(NSString * _Nonnull)error;
+		
+		[Export ("onInitializationFailedWithError:")]
+		void OnInitializationFailedWithError (string error);
+	}
+
+	// @protocol OnRewardEarnedListener
+	/*
+  Check whether adding [Model] to this declaration is appropriate.
+  [Model] is used to generate a C# class that implements this protocol,
+  and might be useful for protocols that consumers are supposed to implement,
+  since consumers can subclass the generated class instead of implementing
+  the generated interface. If consumers are not supposed to implement this
+  protocol, then [Model] is redundant and will generate code that will never
+  be used.
+*/
+	[Protocol (Name = "_TtP12AdMobWrapper22OnRewardEarnedListener_")]
+	[Model(Name="OnRewardEarnedListener")]
+[BaseType(typeof(NSObject))]
+	public interface OnRewardEarnedListener
+	{
+		// @required -(void)onRewardEarnedWithType:(NSString * _Nonnull)type amount:(NSInteger)amount;
+		[Export ("onRewardEarnedWithType:amount:")]
+		void Amount (string type, nint amount);
+	}
+
 	// @interface AdMobManager : NSObject
 	[BaseType (typeof(NSObject), Name = "_TtC12AdMobWrapper12AdMobManager")]
 	[DisableDefaultCtor]
-	interface AdMobManager
+	public interface AdMobManager
 	{
 		// @property (readonly, nonatomic, strong, class) AdMobManager * _Nonnull instance;
 		[Static]
@@ -26,7 +128,7 @@ namespace AMDevIT.Admob.Wrapper
 
 	// @interface AppOpenAdWrapper : NSObject
 	[BaseType (typeof(NSObject), Name = "_TtC12AdMobWrapper16AppOpenAdWrapper")]
-	interface AppOpenAdWrapper
+	public interface AppOpenAdWrapper
 	{
 		// -(void)loadWithAdUnitId:(NSString * _Nonnull)adUnitId loadListener:(id<OnAdLoadedListener> _Nonnull)loadListener eventListener:(id<OnAdEventListener> _Nullable)eventListener;
 		[Export ("loadWithAdUnitId:loadListener:eventListener:")]
@@ -47,7 +149,7 @@ namespace AMDevIT.Admob.Wrapper
 
 	// @interface BannerAdWrapper : NSObject
 	[BaseType (typeof(NSObject), Name = "_TtC12AdMobWrapper15BannerAdWrapper")]
-	interface BannerAdWrapper
+	public interface BannerAdWrapper
 	{
 		// -(UIView * _Nonnull)loadWithAdUnitId:(NSString * _Nonnull)adUnitId viewController:(UIViewController * _Nonnull)viewController loadListener:(id<OnAdLoadedListener> _Nonnull)loadListener eventListener:(id<OnAdEventListener> _Nullable)eventListener __attribute__((warn_unused_result("")));
 		[Export ("loadWithAdUnitId:viewController:loadListener:eventListener:")]
@@ -60,7 +162,7 @@ namespace AMDevIT.Admob.Wrapper
 
 	// @interface InterstitialAdWrapper : NSObject
 	[BaseType (typeof(NSObject), Name = "_TtC12AdMobWrapper21InterstitialAdWrapper")]
-	interface InterstitialAdWrapper
+	public interface InterstitialAdWrapper
 	{
 		// -(void)loadWithAdUnitId:(NSString * _Nonnull)adUnitId loadListener:(id<OnAdLoadedListener> _Nonnull)loadListener eventListener:(id<OnAdEventListener> _Nullable)eventListener;
 		[Export ("loadWithAdUnitId:loadListener:eventListener:")]
@@ -75,113 +177,9 @@ namespace AMDevIT.Admob.Wrapper
 		bool IsLoaded { get; }
 	}
 
-	
-
-	// @protocol OnAdEventListener
-	/*
-  Check whether adding [Model] to this declaration is appropriate.
-  [Model] is used to generate a C# class that implements this protocol,
-  and might be useful for protocols that consumers are supposed to implement,
-  since consumers can subclass the generated class instead of implementing
-  the generated interface. If consumers are not supposed to implement this
-  protocol, then [Model] is redundant and will generate code that will never
-  be used.
-*/[Protocol (Name = "_TtP12AdMobWrapper17OnAdEventListener_")]
-	interface OnAdEventListener
-	{
-		// @required -(void)onAdShown;
-		[Abstract]
-		[Export ("onAdShown")]
-		void OnAdShown ();
-
-		// @required -(void)onAdDismissed;
-		[Abstract]
-		[Export ("onAdDismissed")]
-		void OnAdDismissed ();
-
-		// @required -(void)onAdClicked;
-		[Abstract]
-		[Export ("onAdClicked")]
-		void OnAdClicked ();
-
-		// @required -(void)onAdImpression;
-		[Abstract]
-		[Export ("onAdImpression")]
-		void OnAdImpression ();
-
-		// @required -(void)onAdFailedToShowWithErrorCode:(NSInteger)errorCode errorMessage:(NSString * _Nonnull)errorMessage;
-		[Abstract]
-		[Export ("onAdFailedToShowWithErrorCode:errorMessage:")]
-		void OnAdFailedToShowWithErrorCode (nint errorCode, string errorMessage);
-	}
-
-	// @protocol OnAdLoadedListener
-	/*
-  Check whether adding [Model] to this declaration is appropriate.
-  [Model] is used to generate a C# class that implements this protocol,
-  and might be useful for protocols that consumers are supposed to implement,
-  since consumers can subclass the generated class instead of implementing
-  the generated interface. If consumers are not supposed to implement this
-  protocol, then [Model] is redundant and will generate code that will never
-  be used.
-*/[Protocol (Name = "_TtP12AdMobWrapper18OnAdLoadedListener_")]
-	interface OnAdLoadedListener
-	{
-		// @required -(void)onAdLoaded;
-		[Abstract]
-		[Export ("onAdLoaded")]
-		void OnAdLoaded ();
-
-		// @required -(void)onAdFailedToLoadWithErrorCode:(NSInteger)errorCode errorMessage:(NSString * _Nonnull)errorMessage;
-		[Abstract]
-		[Export ("onAdFailedToLoadWithErrorCode:errorMessage:")]
-		void OnAdFailedToLoadWithErrorCode (nint errorCode, string errorMessage);
-	}
-
-	// @protocol OnInitializedListener
-	/*
-  Check whether adding [Model] to this declaration is appropriate.
-  [Model] is used to generate a C# class that implements this protocol,
-  and might be useful for protocols that consumers are supposed to implement,
-  since consumers can subclass the generated class instead of implementing
-  the generated interface. If consumers are not supposed to implement this
-  protocol, then [Model] is redundant and will generate code that will never
-  be used.
-*/[Protocol (Name = "_TtP12AdMobWrapper21OnInitializedListener_")]
-	interface OnInitializedListener
-	{
-		// @required -(void)onInitialized;
-		[Abstract]
-		[Export ("onInitialized")]
-		void OnInitialized ();
-
-		// @required -(void)onInitializationFailedWithError:(NSString * _Nonnull)error;
-		[Abstract]
-		[Export ("onInitializationFailedWithError:")]
-		void OnInitializationFailedWithError (string error);
-	}
-
-	// @protocol OnRewardEarnedListener
-	/*
-  Check whether adding [Model] to this declaration is appropriate.
-  [Model] is used to generate a C# class that implements this protocol,
-  and might be useful for protocols that consumers are supposed to implement,
-  since consumers can subclass the generated class instead of implementing
-  the generated interface. If consumers are not supposed to implement this
-  protocol, then [Model] is redundant and will generate code that will never
-  be used.
-*/[Protocol (Name = "_TtP12AdMobWrapper22OnRewardEarnedListener_")]
-	interface OnRewardEarnedListener
-	{
-		// @required -(void)onRewardEarnedWithType:(NSString * _Nonnull)type amount:(NSInteger)amount;
-		[Abstract]
-		[Export ("onRewardEarnedWithType:amount:")]
-		void Amount (string type, nint amount);
-	}
-
 	// @interface RewardedAdWrapper : NSObject
 	[BaseType (typeof(NSObject), Name = "_TtC12AdMobWrapper17RewardedAdWrapper")]
-	interface RewardedAdWrapper
+	public interface RewardedAdWrapper
 	{
 		// -(void)loadWithAdUnitId:(NSString * _Nonnull)adUnitId loadListener:(id<OnAdLoadedListener> _Nonnull)loadListener eventListener:(id<OnAdEventListener> _Nullable)eventListener;
 		[Export ("loadWithAdUnitId:loadListener:eventListener:")]
