c1978758e8b1a44752d59948432888f6af49b90a
Alessandro Morvillo <a.morvillo@amdev.it>
2026-04-21 22:40:56 +0200

    Added the first extension methods for iOS

---
+++ sources/dotnet/AMDevIT.Admob.Wrapper/AMDevIT.Admob.Wrapper/Extensions/iOSNative/AdMobManagerExtensions.iOSNative.cs
@@ -0,0 +1,40 @@
+﻿#if IOS
+
+using AMDevIT.Admob.Wrapper.iOSNative;
+
+namespace AMDevIT.Admob.Wrapper.Extensions.iOSNative;
+
+public static class AdMobManagerExtensions
+{
+    public static Task InitializeAsync(this AdMobManager manager, UIViewController viewController)
+    {
+        TaskCompletionSource tcs = new ();
+        manager.InitializeWithViewController(viewController, new InitListener(tcs));
+        return tcs.Task;
+    }
+
+
+    #region Nested classes
+
+    private class InitListener(TaskCompletionSource tcs) 
+        : NSObject, IOnInitializedListener
+    {
+        #region Fields
+
+        private readonly TaskCompletionSource tcs = tcs;
+
+        #endregion
+
+        #region Methods
+
+        public void OnInitialized() => this.tcs.SetResult();
+        public void OnInitializationFailedWithError(string error) =>
+            this.tcs.SetException(new AdException(-1, error));
+
+        #endregion
+    }
+
+    #endregion
+}
+
+#endif
