fd086800970eca8733d7aa70ff0a1f86b0c2ec89
Alessandro Morvillo <a.morvillo@amdev.it>
2026-05-11 13:09:06 +0200

    ViewModel base
    Updated viewmodel base

--- sources/dotnet/AMDevIT.Admob.Wrapper/AMDevIT.Admob.Wrapper.MAUITestApp/ViewModels/ViewModelBase.cs
+++ sources/dotnet/AMDevIT.Admob.Wrapper/AMDevIT.Admob.Wrapper.MAUITestApp/ViewModels/ViewModelBase.cs
@@ -1,8 +1,15 @@
 using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.Logging;
 
 namespace AMDevIT.Admob.Wrapper.MAUITestApp.ViewModels;
 
-public class ViewModelBase
+public class ViewModelBase(ILogger logger)
+    : ObservableObject
 {
+    #region Properties
 
+    protected ILogger Logger => logger;
+
+    #endregion
 }
